b892a5503ea35f90395c2a3b62cb83ae34e6eaa8
Orestarxx <oresto322@gmail.com>
2024-08-10 00:22:44 +0300

    done homework 3

--- homeworks/homework_three/additional/add_first/main_add1.js
+++ homeworks/homework_three/additional/add_first/main_add1.js
@@ -45,7 +45,7 @@ console.log('*********************');
 let numbers2 = [2,17,13,6,22,31,45,66,100,-18];
 let y = 0;
 while (y <numbers2.length){
-    if(numbers2[y] % 2 !== 0){
+    if(y % 2 !== 0){
         console.log(numbers2[y]);
     }
     y++;

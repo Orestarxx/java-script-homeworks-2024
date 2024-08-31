234092b083e6a4c5f9cc8231fb64276f7588d8e0
Orestarxx <oresto322@gmail.com>
2024-08-31 12:33:39 +0300

    doing homework eleven

--- homeworks/homework_eleven/main_homework_eleven.js
+++ homeworks/homework_eleven/main_homework_eleven.js
@@ -6,5 +6,23 @@ let dummy = {
 };
 let {users,carts,recipes} = dummy;
 // - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+fetch(`${urls}${carts}`)
+    .then(value => value.json())
+    .then(carts =>{
+        const arrayOfCarts = carts.carts;
+        console.log(arrayOfCarts);
+        for (const cart of arrayOfCarts) {
+            let divCart = document.createElement('div');
+            divCart.innerText = `${cart.id}`;
+            for (const product of cart.products) {
+                console.log(product);
+                let imgProduct = document.createElement('img');
+                imgProduct.src =`${product.thumbnail}`
+                divCart.append(imgProduct)
 
+            }
+            document.body.append(divCart);
+
+        }
+    })
 //     - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.

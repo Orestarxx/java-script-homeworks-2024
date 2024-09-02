e3f16b1cb714af624c973957580f58cf5ee9e691
Orestarxx <oresto322@gmail.com>
2024-09-02 13:01:42 +0300

    doing homework eleven

--- homeworks/homework_eleven/main_homework_eleven.js
+++ homeworks/homework_eleven/main_homework_eleven.js
@@ -1,4 +1,4 @@
-let urls = new URL('https://dummyjson.com/');
+let url = new URL('https://dummyjson.com/');
 let dummy = {
     carts:'carts',
     recipes:'recipes',
@@ -6,23 +6,16 @@ let dummy = {
 };
 let {users,carts,recipes} = dummy;
 // - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
-fetch(`${urls}${carts}`)
-    .then(value => value.json())
-    .then(carts =>{
-        const arrayOfCarts = carts.carts;
-        console.log(arrayOfCarts);
-        for (const cart of arrayOfCarts) {
-            let divCart = document.createElement('div');
-            divCart.innerText = `${cart.id}`;
-            for (const product of cart.products) {
-                console.log(product);
-                let imgProduct = document.createElement('img');
-                imgProduct.src =`${product.thumbnail}`
-                divCart.append(imgProduct)
-
-            }
-            document.body.append(divCart);
-
+fetch(`${url}${users}`)
+    .then(response => response.json())
+    .then(({users:arrayOfUsers}) => {
+        console.log(arrayOfUsers);
+        let usersHolder = document.createElement('div');
+        document.body.append(usersHolder)
+        for (const user of arrayOfUsers) {
+            
         }
     })
+
+
 //     - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
